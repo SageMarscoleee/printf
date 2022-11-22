02f83e7721a7ff9b203ee9d9530a2a818d72f2d4
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-11-22 05:54:06 +0000

    printf

--- test_file/_putchar.c
+++ test_file/_putchar.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-/* putchar - function to return character to stdout */
+/* _putchar - function to return character to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned */
 
 int _putchar(char c)
 {
